b8b58cc8133ea76250074ddaee31941d72c34885
jiro.kirameki <deziyama0404@gmail.com>
2017-11-07 10:32:42 +0000

    reset

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ root to: 'toppages#index'
 get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
+
+
 get 'signup', to: 'users#new'
 
 resources :tasks
