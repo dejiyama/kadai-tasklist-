0cb1cdd2871e6e94a0b9e5b10397179c0b598309
jiro.kirameki <deziyama0404@gmail.com>
2017-11-08 03:12:28 +0000

    restart

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only:[:destroy]
+  before_action :correct_user, only:[:destroy, :index, :show]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
    def index
+     @task.index
      @tasks = Task.order(created_at: :desc).page(params[:page]).per(3)
   end
 
@@ -15,13 +16,13 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.build(task_params)
-    
     if@task.save
       flash[:success] = 'Taskが正常に投稿されました。'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Taskが投稿されませんでした。'
-      render :new
+      render 'toppages/index'
     end
   end
 
@@ -41,7 +42,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to @task
+    redirect_back(fallback_location: root_path)
 end
 
 private
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20171106072357) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "status"
-    t.string   "title"
     t.integer  "user_id"
   end
 
